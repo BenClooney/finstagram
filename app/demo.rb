6bff8829353ea22ac1e979b086c7dd1cb59dd9cc
Ben Clooney <benjamin.clooney@gmail.com>
2024-10-22 00:32:56 +0000

    W6D1

--- app/demo.rb
+++ app/demo.rb
@@ -1,2 +1,24 @@
+class Human 
+    def hello 
+        puts "hell i am human"
+    end
+end
 
 
+class Robot
+     def hello 
+        puts "hell i am robot"
+     end
+end
+
+class SuperHuman
+     def hello 
+        puts "i am batman"
+     end
+end
+
+
+
+Ben = Human.new
+
+Ben.hello
